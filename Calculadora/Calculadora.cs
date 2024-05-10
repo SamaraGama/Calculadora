8e4305f80102a6284994148b66a68d7e432cb6a9
samaragama <samaragamaferreirads@gmail.com>
2024-05-09 23:43:22 -0300

    add divisao functionality to Calculadora :sparkles:

--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -8,9 +8,10 @@ namespace Calculadora
         {
             switch(operacao.operador)
             {
-                case '+': operacao.resultado= soma(operacao);break;
+                case '+': operacao.resultado = soma(operacao);break;
                 case '-': operacao.resultado = subtracao(operacao);break;
                 case '*': operacao.resultado = multiplicacao(operacao);break;
+                case '/': operacao.resultado = divisao(operacao);break;
                 default: operacao.resultado = 0; break;
             }
             return operacao;
@@ -27,6 +28,9 @@ namespace Calculadora
         {
             return operacao.valorA * operacao.valorB;
         }
-       
+        public int divisao(Operacoes operacao)
+        {
+            return operacao.valorA / operacao.valorB;
+        }
     }
 }
