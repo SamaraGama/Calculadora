e10e6e5e60f126c569fae11e1ddfb697d8262cbc
samaragama <samaragamaferreirads@gmail.com>
2024-05-10 00:52:39 -0300

    print next operations and all operations results :sparkles:

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 
 namespace Calculadora
 {
@@ -8,19 +9,36 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            Queue<Operacoes> filaOperacoes = new Queue<Operacoes>();
+            Queue<Operacoes> filaOperacoes = new();
+            Stack<BigInteger> valorOperacoesProcessadas = new();
+
             filaOperacoes.Enqueue(new Operacoes { valorA = 2, valorB = 3, operador = '+' });
             filaOperacoes.Enqueue(new Operacoes { valorA = 14, valorB = 8, operador = '-' });
             filaOperacoes.Enqueue(new Operacoes { valorA = 5, valorB = 6, operador = '*' });
             filaOperacoes.Enqueue(new Operacoes { valorA = 2147483647, valorB = 2, operador = '+' });
             filaOperacoes.Enqueue(new Operacoes { valorA = 18, valorB = 3, operador = '/' });
 
-            Calculadora calculadora = new Calculadora();
+            Calculadora calculadora = new();
 
-            foreach (var operacao in filaOperacoes)
+            while (filaOperacoes.Count > 0)
             {
+                Operacoes operacao = filaOperacoes.Dequeue();
                 calculadora.calcular(operacao);
+
                 Console.WriteLine("{0} {1} {2} = {3}", operacao.valorA, operacao.operador, operacao.valorB, operacao.resultado);
+
+                valorOperacoesProcessadas.Push(operacao.resultado);
+
+                foreach (var proximaOperacao in filaOperacoes)
+                {
+                    Console.WriteLine("{0} {1} {2}", proximaOperacao.valorA, proximaOperacao.operador, proximaOperacao.valorB);
+                }
+                Console.WriteLine();
+            }
+
+            foreach (var valorOperacaoProcessada in valorOperacoesProcessadas)
+            {
+                Console.WriteLine(valorOperacaoProcessada);
             }
         }
     }
