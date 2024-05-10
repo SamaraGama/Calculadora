e1bc94354fdf1aa67fd7c323adc7b9386d06dea9
samaragama <samaragamaferreirads@gmail.com>
2024-05-09 22:28:30 -0300

    format Operacoes class :art:

--- Calculadora/Operacoes.cs
+++ Calculadora/Operacoes.cs
@@ -3,10 +3,9 @@ namespace Calculadora
 {
     public class Operacoes
     {
-        public int valorA {get;set;}
+        public int valorA { get; set; }
         public char operador { get; set; }
         public int valorB { get; set; }
         public decimal resultado { get; set; }
-
     }
 }
